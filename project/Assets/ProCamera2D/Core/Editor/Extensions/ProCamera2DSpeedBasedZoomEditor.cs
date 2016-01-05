8c07714a33bf732c1733783fb6ae258a7aff3205
songdu <sdsunrunner@126.com>
2016-01-05 18:48:22 +0800

    new project
    new project

---
+++ project/Assets/ProCamera2D/Core/Editor/Extensions/ProCamera2DSpeedBasedZoomEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Com.LuisPedroFonseca.ProCamera2D
+{
+    [CustomEditor(typeof(ProCamera2DSpeedBasedZoom))]
+    public class ProCamera2DSpeedBasedZoomEditor : Editor
+    {
+        void OnEnable()
+        {
+            ProCamera2DEditorHelper.AssignProCamera2D(target as BasePC2D);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var proCamera2DSpeedBasedZoom = (ProCamera2DSpeedBasedZoom)target;
+
+            if(proCamera2DSpeedBasedZoom.ProCamera2D == null)
+                EditorGUILayout.HelpBox("ProCamera2D is not set.", MessageType.Error, true);
+
+            DrawDefaultInspector();
+
+            if (proCamera2DSpeedBasedZoom.ZoomInSmoothness < .5f)
+                proCamera2DSpeedBasedZoom.ZoomInSmoothness = .5f;
+
+            if (proCamera2DSpeedBasedZoom.ZoomOutSmoothness < .5f)
+                proCamera2DSpeedBasedZoom.ZoomOutSmoothness = .5f;
+
+            if (proCamera2DSpeedBasedZoom.MaxZoomInAmount < 1f)
+                proCamera2DSpeedBasedZoom.MaxZoomInAmount = 1f;
+
+            if (proCamera2DSpeedBasedZoom.MaxZoomOutAmount < 1f)
+                proCamera2DSpeedBasedZoom.MaxZoomOutAmount = 1f;
+
+            if (proCamera2DSpeedBasedZoom.SpeedForZoomOut <= proCamera2DSpeedBasedZoom.SpeedForZoomIn)
+                proCamera2DSpeedBasedZoom.SpeedForZoomOut = proCamera2DSpeedBasedZoom.SpeedForZoomIn + .1f;
+
+            if (proCamera2DSpeedBasedZoom.SpeedForZoomIn >= proCamera2DSpeedBasedZoom.SpeedForZoomOut)
+                proCamera2DSpeedBasedZoom.SpeedForZoomIn = proCamera2DSpeedBasedZoom.SpeedForZoomOut - .1f;
+
+            if (proCamera2DSpeedBasedZoom.SpeedForZoomIn < .5f)
+                proCamera2DSpeedBasedZoom.SpeedForZoomIn = .5f;
+        }
+    }
+}
---
+++ project/Assets/ProCamera2D/Core/Editor/Triggers/ProCamera2DTriggerRailsEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Com.LuisPedroFonseca.ProCamera2D
+{
+    [CustomEditor(typeof(ProCamera2DTriggerRails))]
+    [CanEditMultipleObjects]
+    public class ProCamera2DTriggerRailsEditor : Editor
+    {
+        void OnEnable()
+        {
+            ProCamera2DEditorHelper.AssignProCamera2D(target as BasePC2D);
+
+            var proCamera2DTriggerRails = (ProCamera2DTriggerRails)target;
+
+            if(proCamera2DTriggerRails.ProCamera2D != null && proCamera2DTriggerRails.ProCamera2DRails == null)
+                proCamera2DTriggerRails.ProCamera2DRails = proCamera2DTriggerRails.ProCamera2D.GetComponentInChildren<ProCamera2DRails>();
+        }
+    }
+}
